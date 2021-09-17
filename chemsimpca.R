23098354c2b496b0c1b57c1b246ad9d924596f9a
Dinesh Barupal <barupal@gmail.com>
2021-09-17 00:08:42 -0400

    Update chemsimpca.R

--- chemsimpca.R
+++ chemsimpca.R
@@ -8,6 +8,7 @@ hex <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "
 bin <- c("0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111")
 subkeys <- phthalate_compounds$Fingerprint2D
 m <- t(sapply(subkeys,function(x) { as.integer(strsplit(paste(sapply(strsplit(paste(base64Decode(x,"raw")[5:115],collapse=""),"")[[1]],function(x) {bin[which(hex==x)]}),collapse=""),"")[[1]][1:881] ) }))
+write.csv(m, "bitmat.csv", row.names = F, quote = T)
 mat <- m%*%t(m)
 len <- length(m[,1])
 s <- mat.or.vec(len,len)
@@ -16,6 +17,7 @@ for (i in 1:len) {
     s[i,j] <- mat[i,j]/(mat[i,i]+mat[j,j]-mat[i,j])
   }
 }
+write.csv(s, "tmsim.csv", row.names = F, quote = T)
 pca.res <- prcomp(s)
 pca.res.sum <- summary(pca.res)
 df1 <- cbind(phthalate_compounds, pca.res$x[,1:2])
